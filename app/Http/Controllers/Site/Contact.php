3d8c8c941d6549356a2e5ab2227ab601df0e7c09
henriqueviana <henriquehvisa@gmail.com>
2018-01-23 00:29:51 -0200

    Create first Controller, View and Routes

---
+++ app/Http/Controllers/Site/Contact.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Contact extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index() 
+    {
+        
+    }
+}
