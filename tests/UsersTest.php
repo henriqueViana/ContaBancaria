bf521eaec3dfc1f891a3cf0896593cc7d93f41d5
henriqueviana <henriquehvisa@gmail.com>
2018-01-23 08:21:46 -0200

    Unit Tests by Relationship Tables

--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\User;
+use App\Model\Balance;
 
 class UsersTest extends TestCase
 {
@@ -18,5 +19,22 @@ class UsersTest extends TestCase
         ]);
 
         $this->seeInDatabase('users', ['email' => 'tester@tester.com']);
+    }
+
+    public function testRelationshipWithBalancesTable()
+    {
+        $user = User::create([
+            'name' => 'Tester',
+            'email' => 'tester@tester.com',
+            'password' => bcrypt(123)
+        ]);
+
+        $balance = Balance::create([
+            'user_id' => $user->id,
+            'amount' => 15000.00
+        ]);
+
+        $this->seeInDatabase('users',['email' => 'tester@tester.com']);
+        $this->seeInDatabase('balances', ['user_id' => $user->id, 'amount' => 15000.00]);
     }
 }
